53669cfd125d74fb885928ead959ad4f5f00f17d
Paul Jhon Magbanua <magbanuatx56@gmail.com>
2025-03-15 14:25:21 +0800

    chages random

--- src/config.php
+++ src/config.php
@@ -1,8 +1,1 @@
-<?php
-header("Access-Control-Allow-Origin: *"); // Allow all origins (modify for security)
-header("Content-Type: application/json");
 
-$accessToken = "your-smtpjs-access-token"; // Replace with your actual token
-
-echo json_encode(["token" => $accessToken]);
-?>
